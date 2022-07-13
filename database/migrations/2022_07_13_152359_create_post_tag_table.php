d653968c45211bf67a060412c7c5c74ca63fa577
Muhamad Alfaifi <muhamad.alfaifi@gmail.com>
2022-07-13 18:29:56 +0300

    add initial migrations, models, controllers

---
+++ database/migrations/2022_07_13_152359_create_post_tag_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Linking Table Rules
+        // 1. tables name: tags and posts
+        // 2. singular form: tag and post
+        // 3. arbitrary concatenated with _: tag_post
+        // 4. reordered alphapetically: post_tag
+        
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')->constrained('posts')->cascadeOnDelete();
+            $table->foreignId('tag_id')->constrained('tags')->cascadeOnDelete();
+            $table->timestamps();
+
+            $table->unique(['post_id', 'tag_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tag');
+    }
+};
