69b83b963d68e8111c852f8e208bc55616cd441d
Muhamad Alfaifi <muhamad.alfaifi@gmail.com>
2022-08-05 03:00:04 +0300

    bugfix

--- app/Http/Controllers/DownloadableReportController.php
+++ app/Http/Controllers/DownloadableReportController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DownloadableReportController extends Controller
@@ -15,10 +18,18 @@ class DownloadableReportController extends Controller
     public function __invoke(Request $request)
     {
         $validated = $request->validate([
-            'month' => 'required|integer|between:0,11',
+            'month' => 'required|integer|between:1,12',
             'inline' => 'nullable'
         ]);
 
+        $min = now()->setMonth($validated['month'])->startOfMonth();
+        $max = now()->setMonth($validated['month'])->endOfMonth();
+
+        $posts = Post::whereBetween('created_at', [$min, $max])->get();
+
+        dd($posts);
+
+        $filename = 'example_018.pdf';
         $dest = !array_key_exists('inline', $validated) ? 'D' : 'I';
 
         $pdf = app('tcpdf');
@@ -44,7 +55,7 @@ class DownloadableReportController extends Controller
         $pdf->WriteHTML($htmlcontent, true, 0, true, 0);
 
         //Close and output PDF document
-        $pdf->Output('example_018.pdf', $dest);
+        $pdf->Output($filename, $dest);
         exit;
     }
 }
